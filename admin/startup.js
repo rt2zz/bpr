c9c277522cad9380fa3a7c78c547adefed13fda1
Zack <zack@browniepointer.com>
2013-02-11 17:42:14 -0800

    app-ui integration

--- admin/startup.js
+++ admin/startup.js
@@ -1,11 +1,9 @@
 var fs = require('fs')
 
-console.log('Removing tmp files...')
-var files = fs.readdirSync(process.cwd()+'/public/tmp/')
-console.log(files)
+var files = fs.readdirSync(__dirname+'/public/tmp/')
 
 for (var i = 0; i < files.length; i++) {
-  fs.unlink(process.cwd()+'/public/tmp/'+files[i], function (err) {
+  fs.unlink(__dirname+'/public/tmp/'+files[i], function (err) {
     if (err) console.log('Error unlinking '+files[i]+': ', err);
   });
 }
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,5 +1,9 @@
 var app = require('../app.js')
+var Product = require('../logic/db/product.js')
 
 app.get('/', function(req, res){
-	res.render('home')
+  Product.find({status: "2"}).toArray(function(err, products){
+    res.locals.products = products
+    res.render('home')
+  })
 })
