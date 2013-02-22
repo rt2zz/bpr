294ec7e9a255dd590e5d14464493b94f68a7abb7
Zack <zack@browniepointer.com>
2013-02-21 23:12:16 -0800

    infinte scroller

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,8 +2,17 @@ var app = require('../app.js')
 var Product = require('../logic/db/product.js')
 
 app.get('/', function(req, res){
-  Product.find({status: "2"}).toArray(function(err, products){
+  Product.find({status: "2"}, {limit:4}).toArray(function(err, products){
     res.locals.products = products
     res.render('home')
+  })
+})
+
+app.get('/api/1/products', function(req, res){
+  var skip = req.query.skip
+  var limit = req.query.limit
+  console.log(req.query)
+  Product.find({status: "2"}, {limit:limit, skip:skip}).toArray(function(err, products){
+    res.json(products)
   })
 })
